d7c94ff75878f9de9ebf2491e9c74da8e31c63ba
Arwin <ladoa250@gmail.com>
2025-01-18 22:44:20 -0500

    feat!: Added javadocs to all my files that are not test files, basically all the classes
    feat: Added javadoc files to everything except test files, listed as html files.

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"TimerInterface"},{"p":"<Unnamed>","l":"TypingSpeedTest"}];updateSearchResults();
