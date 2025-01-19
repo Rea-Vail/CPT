206b243e7aea4a23e7117ded5bda9b2a8f4c26ac
Arwin <ladoa250@gmail.com>
2025-01-18 22:50:20 -0500

    fix: update Javadoc comments in TimerInterface for improved clarity and accuracy

--- src/TimerInterface.java
+++ src/TimerInterface.java
@@ -6,17 +6,20 @@
 
  /**
   * This class is a interface for timer that will be used in the TypingTest class and it basically makes a new(fake) data type
-  * @param start() method that will start the timer
-  * @param stop() method that will stop the timer
-  * @param getElapsedTime() method that will get the elapsed time
-  * @param calculateWPM() method that will calculate the words per minute
-  * @return The given time, elasped time and wpm
-  * It works as a way for the user to his/her improvement
+  * - start(): Starts the timer.
+  * - stop(): Stops the timer.
+  * - getElapsedTime(): Gets the elapsed time in milliseconds.
+  * - calculateWPM(int wordCount): Calculates the words per minute based on the word count and elapsed time.
+  * @return The given time, elasped time and wpm It works as a way for the user to his/her improvement
+  *
   **/
-
 public interface TimerInterface {
+    // Starts the timer
     void start();
+    // ends the timer
     void stop();
+    // get the elasped time
     long getElapsedTime();
+    // gets the word per minute
     double calculateWPM(int wordCount);
 }
